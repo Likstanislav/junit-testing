0d079f63efb4e79cad80ef3fb320a286cd77bfd6
likstanislav <likstanislav@yandex.ru>
2023-03-09 23:12:23 +0700

    Adding Assert

--- src/test/java/org/example/MathTest.java
+++ src/test/java/org/example/MathTest.java
@@ -1,15 +1,30 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class MathTest {
 
     @Test
     void summ() {
         Math math = new Math();
-        int res = math.summ(5, 7);
-        assert res == 12 : "Sum don't work!";
+
+        assertEquals(12, math.summ(5, 7), "Addition does't work");
+//        assert res == 12 : "Sum don't work!";
+    }
+    @Test
+    public void testArray() {
+        Math math = new Math();
+        int[] arr = math.setArray(4,6,8,10,0);
+        int[] expected = {4,6,8,10,0};
+        assertArrayEquals(arr,expected);
+    }
+
+    @Test
+    public void testDivide() {
+        Math math = new Math();
+        assertEquals(5, math.divide(10,2), "Division does't work");
+        assertThrows(ArithmeticException.class, () -> math.divide(5,0));
     }
 }
